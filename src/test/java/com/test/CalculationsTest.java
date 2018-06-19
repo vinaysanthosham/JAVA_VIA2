c14c4d6d4861e016b13a3cc41812d0705f9f7c1a
nandakumarpurohit <nandakumarpurohit@gmail.com>
2018-06-19 11:59:52 -0400

    changed test case

--- src/test/java/com/test/CalculationsTest.java
+++ src/test/java/com/test/CalculationsTest.java
@@ -21,7 +21,7 @@ public class CalculationsTest {
 	@Test
 	public void testSquare() {
 		//fail("Not yet implemented"); // TODO
-		assertEquals(90, calc.square(10.0));
+		assertEquals(100, calc.square(10.0));
 	}
 
 }
